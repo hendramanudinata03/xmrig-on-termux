130994d77350e9ab8460895561f08ea01557b864
Hendra Manudinata <hendra@manudinata.me>
2022-01-04 14:43:50 +0700

    Fix typos

--- install.sh
+++ install.sh
@@ -24,9 +24,11 @@ wget https://download.open-mpi.org/release/hwloc/v${HWLOC_VERSION:0:-2}/hwloc-${
 tar -xzvf hwloc-${HWLOC_VERSION}.tar.gz -C src/
 rm -rf hwloc-${HWLOC_VERSION}.tar.gz
 
+cd ${CURRENT_DIR}/src
+
 # Install HWLOC
 mkdir ${CURRENT_DIR}/src/built/hwloc-${HWLOC_VERSION}
-cd src/hwloc-${HWLOC_VERSION}
+cd hwloc-${HWLOC_VERSION}
 ./configure --prefix=${CURRENT_DIR}/src/built/hwloc-${HWLOC_VERSION}
 make -j${CORES}
 make install
@@ -34,12 +36,12 @@ make install
 cd ${CURRENT_DIR}/src
 
 # Clone XMRig and checkout to latest version
-git clone -q https://github.com/xmrig/xmrig.git xmrig-v${XMRIG_VERSION} && cd xmrig-v${XMRIG_VERSION}
+git clone -q https://github.com/xmrig/xmrig.git xmrig-v${XMRIG_VERSION}
 git checkout v${XMRIG_VERSION}
 
 # Install XMRig
 mkdir ${CURRENT_DIR}/src/built/xmrig-v${XMRIG_VERSION}
-cd src/xmrig-v${XMRIG_VERSION}
+cd xmrig-v${XMRIG_VERSION}
 mkdir build && cd build
 cmake .. \
   -DHWLOC_INCLUDE_DIR=/data/data/com.termux/files/home/xmrig-on-termux/src/built/hwloc-${HWLOC_VERSION}/include \
